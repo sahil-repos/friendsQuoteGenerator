1ec39a9bc8b46672ad236446eb269dd9dcc2635a
sahil-repos <47462984+sahil-repos@users.noreply.github.com>
2021-09-06 18:30:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const qBtn = document.getElementById('qBtn')
+
+const quoteEl = document.getElementById('quote')
+const charachterEl = document.getElementById('character')
+
+quoteBtn.addEventListener('click', generateQuote)
+
+generateQuote()
+
+function generateQuote() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  fetch('https://friends-quotes-api.herokuapp.com/quotes/random', config)
+    .then((res) => res.json())
+    .then((data) => {
+      quoteEl.innerHTML = data.quote
+      charachterEl.innerHTML = data.character
+    })
+}
